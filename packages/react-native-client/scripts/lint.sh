d9bd091a5626cfbc75959a0f31495ad70bdc7826
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2024-07-17 11:11:12 +0200

    Configured lint and lint:check for react native project

--- packages/react-native-client/scripts/lint.sh
+++ packages/react-native-client/scripts/lint.sh
@@ -6,13 +6,3 @@ eslint . --ext .ts,.tsx --fix
 
 echo "Running prettier for react-native javascript files \n"
 prettier --write . --ignore-path ./.eslintignore
-
-echo "Running ktlint for react-native android files \n"
-cd android
-ktlint -F **/*.kt
-cd ..
-
-echo "Running swift-format for react-native ios files \n"
-cd ios
-swift-format format -i -r ./*.swift --configuration swift-format-config.json
-cd ..
---
+++ packages/react-native-client/scripts/lint_check.sh
@@ -0,0 +1,5 @@
+echo "Running eslint:check for react-native javascript files \n"
+eslint . --ext .ts,.tsx
+
+echo "Running prettier:check for react-native javascript files \n"
+prettier --check . --ignore-path ./.eslintignore
