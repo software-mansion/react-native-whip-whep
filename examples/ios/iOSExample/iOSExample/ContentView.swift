20ad269c8e3e6309f58aeb9bf646e15f936814cb
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2024-07-24 13:30:00 +0200

    Added WHIP, connecting but not streaming yet

--- examples/ios/iOSExample/iOSExample/ContentView.swift
+++ examples/ios/iOSExample/iOSExample/ContentView.swift
@@ -4,25 +4,30 @@ import WebRTC
 
 struct ContentView: View {
     @StateObject var player = WHEPPlayer(connectionOptions: ConnectionOptions(serverUrl: URL(string: "http://192.168.83.105:8829")!, whepEndpoint: "/whep", authToken: "example"))
+    @StateObject var whipPlayer = WHIPPlayer(connectionOptions: ConnectionOptions(serverUrl: URL(string: "http://192.168.83.105:8829")!, whepEndpoint: "/whip", authToken: "example"))
 
     var body: some View {
         VStack {
+            Text("WHEP:")
             if let videoTrack = player.videoTrack {
                     WebRTCVideoView(videoTrack: videoTrack)
                         .frame(width: 300, height: 300)
                 } else {
                     Text("Ładowanie strumienia...")
                 }
-            Text("kotki")
-        }
-        .onAppear {
-            Task {
-                //await startReceivingVideo()
+            Button("Connect WHEP") {
                 Task {
                     try await player.connect()
                 }
-
             }
+            Text("WHIP:")
+            Button("Connect WHIP") {
+                Task {
+                    whipPlayer.setupLocalMedia()
+                    try await whipPlayer.connect()
+                }
+            }
+            
         }
         
 //        VStack {
