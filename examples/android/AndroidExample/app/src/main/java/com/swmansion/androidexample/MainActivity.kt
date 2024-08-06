a6a8bddc37c5377ab72e8daaee4636e3b85f7728
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2024-08-06 10:02:40 +0200

    Initialized code for Android WHIP, lint fixes

---
+++ examples/android/AndroidExample/app/src/main/java/com/swmansion/androidexample/MainActivity.kt
@@ -0,0 +1,121 @@
+package com.swmansion.androidexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.swmansion.androidexample.ui.theme.AndroidExampleTheme
+import kotlinx.coroutines.launch
+import org.webrtc.RendererCommon
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    enableEdgeToEdge()
+    setContent {
+      AndroidExampleTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+          PlayerView(modifier = Modifier.padding(innerPadding))
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun PlayerView(modifier: Modifier = Modifier) {
+  val context = LocalContext.current
+
+  var isLoading by remember { mutableStateOf(false) }
+  var shouldShowPlayBtn by remember {
+    mutableStateOf(true)
+  }
+
+  val whepPlayer = remember {
+    WHEPPlayer(
+      context,
+      ConnectionOptions(serverUrl = "http://192.168.83.63:8829/", whepEndpoint = "/whep")
+    )
+  }
+
+  var view: WHEPPlayerView? = remember {
+    null
+  }
+
+  DisposableEffect(Unit) {
+    onDispose {
+      whepPlayer.release()
+      view?.release()
+    }
+  }
+
+  val coroutineScope = rememberCoroutineScope()
+
+  fun onPlayBtnClick() {
+    shouldShowPlayBtn = false
+    isLoading = true
+    whepPlayer.onTrackAdded = { isLoading = false }
+    coroutineScope.launch {
+      whepPlayer.connect()
+    }
+  }
+
+  Box {
+    AndroidView(
+      factory = { ctx ->
+        WHEPPlayerView(ctx).apply {
+          player = whepPlayer
+          this.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT)
+          this.setEnableHardwareScaler(true)
+          view = this
+        }
+      },
+      modifier = modifier
+        .fillMaxWidth()
+        .height(400.dp)
+        .align(Alignment.Center)
+    )
+    if (shouldShowPlayBtn) {
+      Button(onClick = { onPlayBtnClick() }, modifier = Modifier.align(Alignment.Center)) {
+        Image(
+          painter = painterResource(id = android.R.drawable.ic_media_play),
+          contentDescription = "play"
+        )
+      }
+    }
+    if (isLoading) {
+      CircularProgressIndicator(
+        modifier = Modifier
+          .width(64.dp)
+          .align(Alignment.Center),
+        color = MaterialTheme.colorScheme.secondary,
+        trackColor = MaterialTheme.colorScheme.surfaceVariant,
+      )
+    }
+  }
+}
--- examples/android/AndroidExample/app/src/main/java/com/swmansion/androidexample/WHEPPlayer.kt
+++ examples/android/AndroidExample/app/src/main/java/com/swmansion/androidexample/WHEPPlayer.kt
@@ -1,4 +1,4 @@
-package com.swmansion.whipwhepdemo
+package com.swmansion.androidexample
 
 import android.content.Context
 import android.util.Log
@@ -27,8 +27,6 @@ import org.webrtc.RtpTransceiver
 import org.webrtc.SessionDescription
 import org.webrtc.VideoTrack
 import java.io.IOException
-import java.net.URI
-import java.net.URL
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
@@ -37,12 +35,10 @@ internal interface WHEPPlayerListener {
   fun onTrackAdded(track: VideoTrack)
 }
 
-internal const val WHEP_TAG = "WHEPClient"
+internal const val TAG = "WHEPClient"
 
-class WHEPPlayer(
-  appContext: Context,
-  private val connectionOptions: ConnectionOptions
-) : PeerConnection.Observer {
+class WHEPPlayer(appContext: Context, private val connectionOptions: ConnectionOptions) :
+  PeerConnection.Observer {
   private val peerConnectionFactory: PeerConnectionFactory
   private val peerConnection: PeerConnection
   internal val eglBase = EglBase.create()
@@ -62,12 +58,11 @@ class WHEPPlayer(
   var onTrackAdded: (() -> Unit)? = null
 
   init {
-    val iceServers =
-      listOf(
-        PeerConnection.IceServer
-          .builder("stun:stun.l.google.com:19302")
-          .createIceServer()
-      )
+    val iceServers = listOf(
+      PeerConnection.IceServer
+        .builder("stun:stun.l.google.com:19302")
+        .createIceServer()
+    )
 
     val config = PeerConnection.RTCConfiguration(iceServers)
 
@@ -76,99 +71,78 @@ class WHEPPlayer(
     config.candidateNetworkPolicy = PeerConnection.CandidateNetworkPolicy.ALL
     config.tcpCandidatePolicy = PeerConnection.TcpCandidatePolicy.DISABLED
 
+
     PeerConnectionFactory.initialize(
       PeerConnectionFactory.InitializationOptions.builder(appContext).createInitializationOptions()
     )
 
-    peerConnectionFactory =
-      PeerConnectionFactory
-        .builder()
-        .setVideoDecoderFactory(DefaultVideoDecoderFactory(eglBase.eglBaseContext))
-        .createPeerConnectionFactory()
+    peerConnectionFactory = PeerConnectionFactory
+      .builder()
+      .setVideoDecoderFactory(DefaultVideoDecoderFactory(eglBase.eglBaseContext))
+      .createPeerConnectionFactory()
 
     peerConnection = peerConnectionFactory.createPeerConnection(config, this)!!
   }
 
   private suspend fun sendSdpOffer(sdpOffer: String) = suspendCoroutine { continuation ->
     val request = Request.Builder()
-      .url(connectionOptions.serverUrl)
+      .url(connectionOptions.serverUrl + connectionOptions.whepEndpoint)
       .post(sdpOffer.toRequestBody())
       .header("Accept", "application/sdp")
       .header("Content-Type", "application/sdp")
       .build()
 
-      client.newCall(request).enqueue(
-        object : Callback {
-          override fun onFailure(
-            call: Call,
-            e: IOException
-          ) {
-            continuation.resumeWithException(e)
-            e.printStackTrace()
-          }
-
-          override fun onResponse(
-            call: Call,
-            response: Response
-          ) {
-            response.use {
-              patchEndpoint = response.headers["location"]
-              continuation.resume(response.body!!.string())
-            }
-          }
-        }
-      )
-    }
-
-  private suspend fun sendCandidate(candidate: IceCandidate) =
-    suspendCoroutine { continuation ->
-      if (patchEndpoint == null) return@suspendCoroutine
-
-      val splitSdp = candidate.sdp.split(" ")
-      val ufrag = splitSdp[splitSdp.indexOf("ufrag") + 1]
-
-      val jsonObject = JSONObject()
-      try {
-        jsonObject.put("candidate", candidate.sdp)
-        jsonObject.put("sdpMLineIndex", candidate.sdpMLineIndex)
-        jsonObject.put("sdpMid", candidate.sdpMid)
-        // TODO: is ufrag necessary or is it just elixir webrtc thing?
-        jsonObject.put("usernameFragment", ufrag)
-      } catch (e: JSONException) {
+    client.newCall(request).enqueue(object : Callback {
+      override fun onFailure(call: Call, e: IOException) {
+        continuation.resumeWithException(e)
         e.printStackTrace()
       }
 
-      val serverUrl = URL(connectionOptions.serverUrl)
-    val requestURL =
-      URI(serverUrl.protocol, null, serverUrl.host, serverUrl.port, patchEndpoint, null, null)
+      override fun onResponse(call: Call, response: Response) {
+        response.use {
+          patchEndpoint = response.headers["location"]
+          continuation.resume(response.body!!.string())
+        }
+      }
+    })
+  }
+
+  private suspend fun sendCandidate(candidate: IceCandidate) = suspendCoroutine { continuation ->
+    if (patchEndpoint == null) return@suspendCoroutine
+
+    val splitSdp = candidate.sdp.split(" ")
+    val ufrag = splitSdp[splitSdp.indexOf("ufrag") + 1]
+
+    val jsonObject = JSONObject()
+    try {
+      jsonObject.put("candidate", candidate.sdp)
+      jsonObject.put("sdpMLineIndex", candidate.sdpMLineIndex)
+      jsonObject.put("sdpMid", candidate.sdpMid)
+      // TODO: is ufrag necessary or is it just elixir webrtc thing?
+      jsonObject.put("usernameFragment", ufrag)
+    } catch (e: JSONException) {
+      e.printStackTrace()
+    }
 
     val request = Request.Builder()
-      .url(requestURL.toURL())
+      .url(connectionOptions.serverUrl + patchEndpoint!!)
       .patch(jsonObject.toString().toRequestBody())
       .header("Content-Type", "application/trickle-ice-sdpfrag")
       .build()
 
-      client.newCall(request).enqueue(
-        object : Callback {
-          override fun onFailure(
-            call: Call,
-            e: IOException
-          ) {
-            continuation.resumeWithException(e)
-            e.printStackTrace()
-          }
+    client.newCall(request).enqueue(object : Callback {
+      override fun onFailure(call: Call, e: IOException) {
+        continuation.resumeWithException(e)
+        e.printStackTrace()
+      }
 
-          override fun onResponse(
-            call: Call,
-            response: Response
-          ) {
-            response.use {
-              continuation.resume(Unit)
-            }
-          }
+      override fun onResponse(call: Call, response: Response) {
+        response.use {
+          continuation.resume(Unit)
         }
-      )
-    }
+      }
+    })
+  }
 
   suspend fun connect() {
     peerConnection.addTransceiver(MediaStreamTrack.MediaType.MEDIA_TYPE_VIDEO).direction =
@@ -180,17 +154,16 @@ class WHEPPlayer(
     val sdpOffer = peerConnection.createOffer(constraints).getOrThrow()
     peerConnection.setLocalDescription(sdpOffer).getOrThrow()
 
-    Log.d(WHEP_TAG, sdpOffer.description)
+    Log.d(TAG, sdpOffer.description)
 
     val sdp = sendSdpOffer(sdpOffer.description)
 
     iceCandidates.forEach { sendCandidate(it) }
 
-    val answer =
-      SessionDescription(
-        SessionDescription.Type.ANSWER,
-        sdp
-      )
+    val answer = SessionDescription(
+      SessionDescription.Type.ANSWER,
+      sdp
+    )
     peerConnection.setRemoteDescription(answer)
   }
 
@@ -201,19 +174,19 @@ class WHEPPlayer(
   }
 
   override fun onSignalingChange(p0: PeerConnection.SignalingState?) {
-    Log.d(WHEP_TAG, "onSignalingChange: $p0")
+    Log.d(TAG, "onSignalingChange: $p0")
   }
 
   override fun onIceConnectionChange(p0: PeerConnection.IceConnectionState?) {
-    Log.d(WHEP_TAG, "onIceConnectionChange: $p0")
+    Log.d(TAG, "onIceConnectionChange: $p0")
   }
 
   override fun onIceConnectionReceivingChange(p0: Boolean) {
-    Log.d(WHEP_TAG, "onIceConnectionReceivingChange: $p0")
+    Log.d(TAG, "onIceConnectionReceivingChange: $p0")
   }
 
   override fun onIceGatheringChange(p0: PeerConnection.IceGatheringState?) {
-    Log.d(WHEP_TAG, "onIceGatheringChange: $p0")
+    Log.d(TAG, "onIceGatheringChange: $p0")
   }
 
   override fun onIceCandidate(candidate: IceCandidate) {
@@ -227,28 +200,26 @@ class WHEPPlayer(
   }
 
   override fun onIceCandidatesRemoved(p0: Array<out IceCandidate>?) {
+
   }
 
   override fun onAddStream(p0: MediaStream?) {
-    Log.d(WHEP_TAG, "onAddStream: $p0")
+    Log.d(TAG, "onAddStream: $p0")
   }
 
   override fun onRemoveStream(p0: MediaStream?) {
-    Log.d(WHEP_TAG, "onRemoveStream: $p0")
+    Log.d(TAG, "onRemoveStream: $p0")
   }
 
   override fun onDataChannel(p0: DataChannel?) {
-    Log.d(WHEP_TAG, "onDataChannel: $p0")
+    Log.d(TAG, "onDataChannel: $p0")
   }
 
   override fun onRenegotiationNeeded() {
-    Log.d(WHEP_TAG, "onRenegotiationNeeded")
+    Log.d(TAG, "onRenegotiationNeeded")
   }
 
-  override fun onAddTrack(
-    receiver: RtpReceiver?,
-    mediaStreams: Array<out MediaStream>?
-  ) {
+  override fun onAddTrack(receiver: RtpReceiver?, mediaStreams: Array<out MediaStream>?) {
     coroutineScope.launch(Dispatchers.Main) {
       val videoTrack = receiver?.track() as? VideoTrack?
       this@WHEPPlayer.videoTrack = videoTrack
