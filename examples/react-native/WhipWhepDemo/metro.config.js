f21ccc3fcffd46eed0d43cb180b0e7848ac16d85
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2024-08-29 14:37:27 +0200

    Fixed example app not starting

--- examples/react-native/WhipWhepDemo/metro.config.js
+++ examples/react-native/WhipWhepDemo/metro.config.js
@@ -1,11 +1,11 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const path = require('path');
+
+const root = path.resolve(__dirname, '../../..');
+
+const config = {
+  watchFolders: [root],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
