152cd6578e41c03ed47b109817fe8a3435e875c9
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2024-08-29 12:34:06 +0200

    Initialized example application

---
+++ examples/react-native/WhipWhepDemo/babel.config.js
@@ -0,0 +1,17 @@
+const pak = require('../../../package.json');
+const path = require('path');
+
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          [pak.name]: path.join(__dirname, '..', 'src/index.tsx'),
+        },
+      },
+    ],
+  ],
+};
