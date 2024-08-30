6a265c8377b12605c88aa9daa2a81d4621e66a78
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2024-08-30 10:21:24 +0200

    Formatting

--- packages/react-native-client/android/src/main/java/com/swmansion/mobilewhepclient/MobileWhepClientView.kt
+++ packages/react-native-client/android/src/main/java/com/swmansion/mobilewhepclient/MobileWhepClientView.kt
@@ -4,4 +4,7 @@ import android.content.Context
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.views.ExpoView
 
-class MobileWhepClientView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
+class MobileWhepClientView(
+  context: Context,
+  appContext: AppContext,
+) : ExpoView(context, appContext)
