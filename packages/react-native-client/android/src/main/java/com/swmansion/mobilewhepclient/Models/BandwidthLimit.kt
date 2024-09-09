4fb11f1fde2a0b88956fa73370df545bfc001296
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2024-09-09 10:19:24 +0200

    Working android

---
+++ packages/react-native-client/android/src/main/java/com/swmansion/mobilewhepclient/Models/BandwidthLimit.kt
@@ -0,0 +1,6 @@
+package com.swmansion.mobilewhepclient
+
+/**
+ * Type describing maximal bandwidth that can be used, in kbps. 0 is interpreted as unlimited bandwidth.
+ */
+typealias BandwidthLimit = Int
