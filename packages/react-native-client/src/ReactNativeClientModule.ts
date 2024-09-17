8983ad9768a35aae75fcd1f3f0a760dc98cd0072
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2024-09-17 11:19:55 +0200

    Added function types to ReactNativeClientModule

--- packages/react-native-client/src/ReactNativeClientModule.ts
+++ packages/react-native-client/src/ReactNativeClientModule.ts
@@ -3,10 +3,28 @@ import {
   requireNativeModule,
   Subscription,
 } from "expo-modules-core";
+import { ConfigurationOptions } from "./ReactNativeClient.types";
+import { NativeModule } from "react-native";
 
-// It loads the native module object from the JSI or falls back to
-// the bridge module (from NativeModulesProxy) if the remote debugger is on.
-const nativeModule = requireNativeModule("ReactNativeClient");
+type RNClientModule = {
+  createWhepClient: (
+    serverUrl: string,
+    configurationOptions?: ConfigurationOptions
+  ) => void;
+  connectWhep: () => Promise<void>;
+  disconnectWhep: () => void;
+  createWhipClient: (
+    serverUrl: string,
+    configurationOptions?: ConfigurationOptions,
+    videoDevice?: string
+  ) => void;
+  connectWhip: () => Promise<void>;
+  disconnectWhip: () => void;
+  getCaptureDevices: () => string[];
+};
+
+const nativeModule: RNClientModule & NativeModule =
+  requireNativeModule("ReactNativeClient");
 export const eventEmitter = new EventEmitter(nativeModule);
 
 export function addTrackListener(listener: (event) => void): Subscription {
