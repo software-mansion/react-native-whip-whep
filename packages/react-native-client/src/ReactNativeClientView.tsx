c78828314d453b9b932482ab6eafa540040f5514
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2024-09-17 12:38:24 +0200

    Added props type to ReactNativeClientView

--- packages/react-native-client/src/ReactNativeClientView.tsx
+++ packages/react-native-client/src/ReactNativeClientView.tsx
@@ -1,13 +1,14 @@
 import { requireNativeViewManager } from "expo-modules-core";
 import * as React from "react";
+import { ReactNativeClientViewProps } from "./ReactNativeClient.types";
 
-const NativeView: React.ComponentType = requireNativeViewManager(
-  "ReactNativeClientViewModule"
-);
+const NativeView: React.ComponentType<ReactNativeClientViewProps> =
+  requireNativeViewManager("ReactNativeClientViewModule");
 
-export const ReactNativeClientView = React.forwardRef<React.ComponentType>(
-  (props, ref) => (
-    // @ts-expect-error ref prop needs to be updated
-    <NativeView {...props} ref={ref} />
-  )
-);
+export const ReactNativeClientView = React.forwardRef<
+  React.ComponentType<ReactNativeClientViewProps>,
+  ReactNativeClientViewProps
+>((props, ref) => (
+  // @ts-expect-error ref prop needs to be updated
+  <NativeView {...props} ref={ref} />
+));
