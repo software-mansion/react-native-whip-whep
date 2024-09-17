b7cd0c3317e5c95fe7e01cd7adfba6df93511d3e
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2024-09-17 11:43:28 +0200

    Cleaned up React Native Module code

--- packages/react-native-client/src/ReactNativeClientModule.ts
+++ packages/react-native-client/src/ReactNativeClientModule.ts
@@ -31,4 +31,43 @@ export function addTrackListener(listener: (event) => void): Subscription {
   return eventEmitter.addListener("trackAdded", listener);
 }
 
+export function createWhepClient(
+  serverUrl: string,
+  configurationOptions?: ConfigurationOptions
+) {
+  return nativeModule.createWhepClient(serverUrl, configurationOptions);
+}
+
+export async function connectWhepClient() {
+  return await nativeModule.connectWhep();
+}
+
+export function disconnectWhepClient() {
+  return nativeModule.disconnectWhep();
+}
+
+export function createWhipClient(
+  serverUrl: string,
+  configurationOptions?: ConfigurationOptions,
+  videoDevice?: string
+) {
+  return nativeModule.createWhipClient(
+    serverUrl,
+    configurationOptions,
+    videoDevice
+  );
+}
+
+export async function connectWhipClient() {
+  return await nativeModule.connectWhip();
+}
+
+export function disconnectWhipClient() {
+  return nativeModule.disconnectWhip();
+}
+
+export function getCaptureDevices() {
+  return nativeModule.getCaptureDevices();
+}
+
 export default nativeModule;
--- packages/react-native-client/src/index.ts
+++ packages/react-native-client/src/index.ts
@@ -7,51 +7,19 @@ import ReactNativeClientModule, {
 } from "./ReactNativeClientModule";
 import { ReactNativeClientView } from "./ReactNativeClientView";
 
-export function createWhepClient(
-  serverUrl: string,
-  configurationOptions?: ConfigurationOptions
-) {
-  return ReactNativeClientModule.createWhepClient(
-    serverUrl,
-    configurationOptions
-  );
-}
-
-export async function connectWhepClient() {
-  return await ReactNativeClientModule.connectWhep();
-}
-
-export function disconnectWhepClient() {
-  return ReactNativeClientModule.disconnectWhep();
-}
-
-export function createWhipClient(
-  serverUrl: string,
-  configurationOptions?: ConfigurationOptions,
-  videoDevice?: string
-) {
-  return ReactNativeClientModule.createWhipClient(
-    serverUrl,
-    configurationOptions,
-    videoDevice
-  );
-}
-
-export async function connectWhipClient() {
-  return await ReactNativeClientModule.connectWhip();
-}
-
-export function disconnectWhipClient() {
-  return ReactNativeClientModule.disconnectWhip();
-}
-
-export function getCaptureDevices() {
-  return ReactNativeClientModule.getCaptureDevices();
-}
-
 export { ReactNativeClientView, ChangeEventPayload, addTrackListener };
 export {
   PlayerType,
   VideoParameters,
   ConfigurationOptions,
 } from "./ReactNativeClient.types";
+
+export {
+  createWhepClient,
+  connectWhepClient,
+  disconnectWhepClient,
+  createWhipClient,
+  connectWhipClient,
+  disconnectWhipClient,
+  getCaptureDevices,
+} from "./ReactNativeClientModule";
