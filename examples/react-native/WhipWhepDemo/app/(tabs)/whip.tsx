740bb0683693f571349956fe8cd5a68a532adc4b
Miron Pawlik <miron.pawlik@swmansion.com>
2024-09-26 16:25:08 +0200

    Update package name

--- examples/react-native/WhipWhepDemo/app/(tabs)/whip.tsx
+++ examples/react-native/WhipWhepDemo/app/(tabs)/whip.tsx
@@ -7,7 +7,7 @@ import {
   createWhipClient,
   disconnectWhipClient,
   WhipClientView,
-} from '@mobile-whep/react-native-client';
+} from 'react-native-whip-whep';
 import { checkPermissions } from '@/utils/CheckPermissions';
 
 export default function HomeScreen() {
