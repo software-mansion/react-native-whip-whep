312be855300684c9857f22fe938ef9ff27351cbd
Anna Olak <15946812+anna1901@users.noreply.github.com>
2025-09-09 15:26:02 +0200

    Add permissions request for ios

--- examples/react-native/WhipWhepDemo/utils/CheckPermissions.ts
+++ examples/react-native/WhipWhepDemo/utils/CheckPermissions.ts
@@ -1,7 +1,18 @@
 import { Platform, PermissionsAndroid } from 'react-native';
+import { Camera } from 'expo-camera';
+import { Audio } from 'expo-av';
 
 export async function checkPermissions() {
   if (Platform.OS === 'ios') {
+    try {
+      const { status: cameraStatus } = await Camera.requestCameraPermissionsAsync();
+      const { status: micStatus } = await Audio.requestPermissionsAsync();
+      if (cameraStatus !== 'granted' || micStatus !== 'granted') {
+        console.warn('Camera/Microphone permissions not granted');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
     return;
   }
   try {
