a807ea4ece5a11e8a943e30dff5b34ed7e9ab2e9
Anna Olak <15946812+anna1901@users.noreply.github.com>
2025-09-12 14:12:58 +0200

    Fix formatting

--- packages/ios-client/Sources/ios-client/Errors.swift
+++ packages/ios-client/Sources/ios-client/Errors.swift
@@ -14,15 +14,15 @@ public enum SessionNetworkError: LocalizedError {
     case CandidateSendingError(description: String)
     case ConnectionError(description: String)
     case ConfigurationError(description: String)
-  
-  public var errorDescription: String? {
-    switch self {
-    case .CandidateSendingError(description: let description):
-      return "Candidate sending error: \(description)"
-    case .ConnectionError(description: let description):
-      return "Connection error: \(description)"
-    case .ConfigurationError(description: let description):
-      return "Configuration error: \(description)"
+
+    public var errorDescription: String? {
+        switch self {
+        case .CandidateSendingError(let description):
+            return "Candidate sending error: \(description)"
+        case .ConnectionError(let description):
+            return "Connection error: \(description)"
+        case .ConfigurationError(let description):
+            return "Configuration error: \(description)"
+        }
     }
-  }
 }
