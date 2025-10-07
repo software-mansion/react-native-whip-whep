9657eba427eb2e66710639b3944ca40fd2544f9c
Anna Olak <15946812+anna1901@users.noreply.github.com>
2025-10-07 14:02:43 +0200

    Add title for main screen

--- examples/react-native/WhipWhepDemo/app/_layout.tsx
+++ examples/react-native/WhipWhepDemo/app/_layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false, title: "Home" }} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
