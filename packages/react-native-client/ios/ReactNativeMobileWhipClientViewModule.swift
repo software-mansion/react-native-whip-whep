1cfd6248fc82ff9fded835a42842b4b419e060bb
Anna Olak <15946812+anna1901@users.noreply.github.com>
2025-10-14 17:02:21 +0200

    Changes to initialize camera after expo 54 bump

--- packages/react-native-client/ios/ReactNativeMobileWhipClientViewModule.swift
+++ packages/react-native-client/ios/ReactNativeMobileWhipClientViewModule.swift
@@ -143,17 +143,18 @@ public class ReactNativeMobileWhipClientViewModule: Module {
 
         View(ReactNativeMobileWhipClientView.self) {
             AsyncFunction("initializeCamera") { (view: ReactNativeMobileWhipClientView, options: ConfigurationOptions) in
-                Task { @MainActor in
-                    guard await PermissionUtils.requestCameraPermission() else {
-                        self.emit(event: .warning(message: "Camera permission not granted."))
-                        return
-                    }
-                    guard await PermissionUtils.requestMicrophonePermission() else {
-                        self.emit(event: .warning(message: "Microphone permission not granted."))
-                        return
-                    }
-                    
-                    try self.createWhipClient(options: options)
+                guard await PermissionUtils.requestCameraPermission() else {
+                    self.emit(event: .warning(message: "Camera permission not granted."))
+                    return
+                }
+                guard await PermissionUtils.requestMicrophonePermission() else {
+                    self.emit(event: .warning(message: "Microphone permission not granted."))
+                    return
+                }
+                
+                try self.createWhipClient(options: options)
+                Task { @MainActor [weak self] in
+                    guard let self else { return }
                     view.player = self.whipClient
                 }
             }
