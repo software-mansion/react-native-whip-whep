81fef5fcbda9af8c7bba20351805dd18b0cca896
Anna Olak <15946812+anna1901@users.noreply.github.com>
2025-10-22 17:07:34 +0200

    Move whip client to view on ios

--- packages/react-native-client/ios/ReactNativeMobileWhipClientViewModule.swift
+++ packages/react-native-client/ios/ReactNativeMobileWhipClientViewModule.swift
@@ -3,8 +3,6 @@ import MobileWhipWhepClient
 import WebRTC
 
 public class ReactNativeMobileWhipClientViewModule: Module {
-    private var whipClient: WhipClient?
-
     struct ConfigurationOptions: Record {
         @Field
         var audioEnabled: Bool?
@@ -29,55 +27,7 @@ public class ReactNativeMobileWhipClientViewModule: Module {
         var authToken: String?
     }
 
-    private func createWhipClient(options: ConfigurationOptions) throws {
-        guard let videoDeviceId = options.videoDeviceId,
-            let avCaptureDevice = AVCaptureDevice(uniqueID: videoDeviceId)
-        else {
-            throw Exception(
-                name: "E_INVALID_VIDEO_DEVICE_ID",
-                description: "Invalid video device ID. Make sure the device ID is correct.")
-        }
-
-        let parsedVideoParameters: VideoParameters
-
-        if let optionsVideoParameters = options.videoParameters,
-            let parameters = try? self.getVideoParametersFromOptions(
-                createOptions: optionsVideoParameters)
-        {
-            parsedVideoParameters = parameters
-        } else {
-            parsedVideoParameters = VideoParameters.presetHD169
-        }
-
-        let options = WhipConfigurationOptions(
-            audioEnabled: options.audioEnabled ?? true,
-            videoEnabled: options.videoEnabled ?? true,
-            videoDevice: avCaptureDevice,
-            videoParameters: parsedVideoParameters,
-            stunServerUrl: options.stunServerUrl,
-            preferredVideoCodecs: options.preferredVideoCodecs ?? [],
-            preferredAudioCodecs: options.preferredAudioCodecs ?? []
-        )
-
-        guard options.videoEnabled, PermissionUtils.hasCameraPermission() else {
-            emit(
-                event: .warning(
-                    message: "Camera permission not granted. Cannot initialize WhipClient."))
-            return
-        }
-
-        guard options.audioEnabled, PermissionUtils.hasMicrophonePermission() else {
-            emit(
-                event: .warning(
-                    message: "Microphone permission not granted. Cannot initialize WhipClient."))
-            return
-        }
-
-        whipClient = WhipClient(configOptions: options)
-        whipClient?.onConnectionStateChanged = { [weak self] newState in
-            self?.emit(event: .whipPeerConnectionStateChanged(status: newState))
-        }
-    }
+    
 
     private func emit(event: WhipEmitableEvent) {
         DispatchQueue.main.async {
@@ -102,36 +52,7 @@ public class ReactNativeMobileWhipClientViewModule: Module {
         }
     }
 
-    private func getVideoParametersFromOptions(createOptions: String) throws -> VideoParameters {
-        let preset: VideoParameters = {
-            switch createOptions {
-            case "QVGA169":
-                return VideoParameters.presetQVGA169
-            case "VGA169":
-                return VideoParameters.presetVGA169
-            case "VQHD169":
-                return VideoParameters.presetQHD169
-            case "HD169":
-                return VideoParameters.presetHD169
-            case "FHD169":
-                return VideoParameters.presetFHD169
-            case "QVGA43":
-                return VideoParameters.presetQVGA43
-            case "VGA43":
-                return VideoParameters.presetVGA43
-            case "VQHD43":
-                return VideoParameters.presetQHD43
-            case "HD43":
-                return VideoParameters.presetHD43
-            case "FHD43":
-                return VideoParameters.presetFHD43
-            default:
-                return VideoParameters.presetVGA169
-            }
-        }()
-        return preset
-    }
-
+    
     public func definition() -> ModuleDefinition {
         Name("ReactNativeMobileWhipClientViewModule")
 
@@ -152,88 +73,33 @@ public class ReactNativeMobileWhipClientViewModule: Module {
                     return
                 }
                 
-                try self.createWhipClient(options: options)
-                Task { @MainActor [weak self] in
-                    guard let self else { return }
-                    view.player = self.whipClient
+                try await view.createWhipClient(options: options) { [weak self] newState in
+                    self?.emit(event: .whipPeerConnectionStateChanged(status: newState))
                 }
             }
 
-            AsyncFunction("connect") { (options: ConnectionOptions) in
-                guard let client = self.whipClient else {
-                    throw Exception(
-                        name: "E_WHIP_CLIENT_NOT_FOUND",
-                        description: "WHIP client not found. Make sure it was initialized properly."
-                    )
-                }
-
-                guard let url = URL(string: options.serverUrl) else {
-                    throw Exception(
-                        name: "E_INVALID_SERVER_URL",
-                        description: "Invalid server URL. Make sure the address is correct.")
-                }
-                try await client.connect(.init(serverUrl: url, authToken: options.authToken))
+            AsyncFunction("connect") { (view: ReactNativeMobileWhipClientView, options: ConnectionOptions) in
+                try await view.connect(options: options)
             }
 
-            AsyncFunction("flipCamera") {
-                guard let client = self.whipClient else {
-                    throw Exception(
-                        name: "E_WHIP_CLIENT_NOT_FOUND",
-                        description: "WHIP client not found. Make sure it was initialized properly."
-                    )
-                }
-
-                guard let currentCameraId = self.whipClient?.currentCameraDeviceId else {
-                    throw Exception(
-                        name: "E_CAMERA_NOT_FOUND",
-                        description: "No camera found.")
-                }
-
-                let cameras = RTCCameraVideoCapturer.captureDevices()
-
-                let currentCamera = cameras.first { device in
-                    device.uniqueID == currentCameraId
-                }
-
-                let oppositeCamera = cameras.first { device in
-                    device.position != currentCamera?.position
-                }
-
-                guard let oppositeCamera else {
-                    throw Exception(
-                        name: "E_CAMERA_NOT_FOUND",
-                        description: "No camera found.")
-                }
-
-                client.switchCamera(deviceId: oppositeCamera.uniqueID)
+            AsyncFunction("flipCamera") { (view: ReactNativeMobileWhipClientView) in
+                try await view.flipCamera()
             }
 
-            AsyncFunction("switchCamera") { (deviceId: String) in
-                guard let client = self.whipClient else {
-                    throw Exception(
-                        name: "E_WHIP_CLIENT_NOT_FOUND",
-                        description: "WHIP client not found. Make sure it was initialized properly."
-                    )
-                }
-                client.switchCamera(deviceId: deviceId)
+            AsyncFunction("switchCamera") { (view: ReactNativeMobileWhipClientView, deviceId: String) in
+                try await view.switchCamera(deviceId: deviceId)
             }
 
-            AsyncFunction("disconnect") {
-                try await self.whipClient?.disconnect()
+            AsyncFunction("disconnect") { (view: ReactNativeMobileWhipClientView) in
+                try await view.disconnect()
             }
 
-            AsyncFunction("cleanup") {
-                self.whipClient?.delegate = nil
-                self.whipClient?.onConnectionStateChanged = nil
-                self.whipClient = nil
+            AsyncFunction("setPreferredSenderVideoCodecs") { (view: ReactNativeMobileWhipClientView, preferredCodecs: [String]?) in
+                view.setPreferredVideoCodecs(preferredCodecs: preferredCodecs)
             }
 
-            AsyncFunction("setPreferredSenderVideoCodecs") { (preferredCodecs: [String]?) in
-                self.whipClient?.setPreferredVideoCodecs(preferredCodecs: preferredCodecs)
-            }
-
-            AsyncFunction("setPreferredSenderAudioCodecs") { (preferredCodecs: [String]?) in
-                self.whipClient?.setPreferredAudioCodecs(preferredCodecs: preferredCodecs)
+            AsyncFunction("setPreferredSenderAudioCodecs") { (view: ReactNativeMobileWhipClientView, preferredCodecs: [String]?) in
+                view.setPreferredAudioCodecs(preferredCodecs: preferredCodecs)
             }
 
             AsyncFunction("getSupportedSenderVideoCodecsNames") {
@@ -244,8 +110,8 @@ public class ReactNativeMobileWhipClientViewModule: Module {
                 WhipClient.getSupportedSenderAudioCodecsNames()
             }
 
-            AsyncFunction("currentCameraDeviceId") {
-                self.whipClient?.currentCameraDeviceId
+            AsyncFunction("currentCameraDeviceId") { (view: ReactNativeMobileWhipClientView) in
+                view.getCurrentCameraDeviceId()
             }
         }
     }
